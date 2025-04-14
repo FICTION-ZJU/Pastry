88a7615aba2fb414dadab422fb7825623a95d141
V-star-ai <mingqiyang@zju.edu.cn>
2025-04-15 00:42:18 +0800

    Update bounded.py

--- pastry/src/transformers/bounded.py
+++ pastry/src/transformers/bounded.py
@@ -6,11 +6,6 @@ import sympy as sp
 def get_bounded_coeffs(var_comp_dict, var_bound_dict, M_str):
     """
     Calculate intermediate results, preparing for the convertion process.
-    
-    :param var_comp_dict: A dictionary mapping each variable to the minimum value that must be added to make it non-negative.
-    :param var_bound_dict: A dictionary mapping each variable to the bound.
-    :param M_str: The unbounded variable. If no unbounded variable exists, this value will be None.
-    :return: intermediate results used for the convertion process.
     """
     var_coeff_dict = {}
     var_sub_dict = {}
@@ -26,7 +21,7 @@ def get_bounded_coeffs(var_comp_dict, var_bound_dict, M_str):
             var_sub_dict[var] = DIV(z_bd, cum_coeff) - comp
         else:
             var_sub_dict[var] = DIV((MOD(z_bd, ne_cum_coeff) - cum_expr), cum_coeff) - comp
-            cum_expr += cum_coeff * (sp.Symbol(var) + comp)
+            cum_expr += (cum_coeff * (sp.Symbol(var) + comp)).subs(sp.Symbol(var), var_sub_dict[var])
         cum_coeff = ne_cum_coeff
 
     if M_str:
@@ -34,17 +29,9 @@ def get_bounded_coeffs(var_comp_dict, var_bound_dict, M_str):
         var_sub_dict[M_str] = DIV(z_bd, cum_coeff)
     return var_coeff_dict, var_sub_dict
 
-
 def convert_bounded_AST(syntax_tree, replacement_map, var_coeff_dict, var_sub_dict):
     """
-    Convert the syntax tree of a bounded k-d PCP to a 1-d PCP syntax tree by replacing guards
-    and assignments.
-    
-    :param syntax_tree: The syntax tree of the bounded k-d PCP to be transformed.
-    :param replacement_map: A dictionary mapping guard labels (e.g., "guard_0") to their original symbolic expressions.
-    :param var_coeff_dict: A dictionary mapping variables to the coefficients, used for updating assignments.
-    :param var_sub_dict: A dictionary for variable substitutions, used for updating guard expressions.
-    :return:
+    Convert the syntax tree of a bounded k-d PCP to a 1-d PCP syntax tree by replacing guards and assignments.
     """
     if isinstance(syntax_tree, list):
         for item in syntax_tree:
@@ -52,23 +39,18 @@ def convert_bounded_AST(syntax_tree, replacement_map, var_coeff_dict, var_sub_di
 
     elif isinstance(syntax_tree, WhileInstr):
         init_guard = replacement_map[syntax_tree.cond.var]
-        syntax_tree.cond = GuardExpr(init_guard.subs(var_sub_dict)) if init_guard.free_symbols else GuardExpr(init_guard)
-        for body in syntax_tree.body:
-            convert_bounded_AST(body, replacement_map, var_coeff_dict, var_sub_dict)
+        syntax_tree.cond = GuardExpr(init_guard.subs(var_sub_dict))
+        convert_bounded_AST(syntax_tree.body, replacement_map, var_coeff_dict, var_sub_dict)
 
     elif isinstance(syntax_tree, IfInstr):
         init_guard = replacement_map[syntax_tree.cond.var]
-        syntax_tree.cond = GuardExpr(init_guard.subs(var_sub_dict)) if init_guard.free_symbols else GuardExpr(init_guard)
-        for body in syntax_tree.true:
-            convert_bounded_AST(body, replacement_map, var_coeff_dict, var_sub_dict)
-        for body in syntax_tree.false:
-            convert_bounded_AST(body, replacement_map, var_coeff_dict, var_sub_dict)
+        syntax_tree.cond = GuardExpr(init_guard.subs(var_sub_dict))
+        convert_bounded_AST(syntax_tree.true, replacement_map, var_coeff_dict, var_sub_dict)
+        convert_bounded_AST(syntax_tree.false, replacement_map, var_coeff_dict, var_sub_dict)
 
     elif isinstance(syntax_tree, ChoiceInstr):
-        for body in syntax_tree.lhs:
-            convert_bounded_AST(body, replacement_map, var_coeff_dict, var_sub_dict)
-        for body in syntax_tree.rhs:
-            convert_bounded_AST(body, replacement_map, var_coeff_dict, var_sub_dict)
+        convert_bounded_AST(syntax_tree.lhs, replacement_map, var_coeff_dict, var_sub_dict)
+        convert_bounded_AST(syntax_tree.rhs, replacement_map, var_coeff_dict, var_sub_dict)
 
     elif isinstance(syntax_tree, AsgnInstr):
         ovar = syntax_tree.lhs
@@ -76,22 +58,21 @@ def convert_bounded_AST(syntax_tree, replacement_map, var_coeff_dict, var_sub_di
         syntax_tree.rhs.lhs = VarExpr('z_bd')
         syntax_tree.rhs.rhs = NatLitExpr(int(syntax_tree.rhs.rhs.value * var_coeff_dict[ovar]))
 
-
-def convert_bounded_pcp(sd_pgcl_prog, replacement_map, var_comp_dict, var_bound_dict, M_str):
+def convert_bounded_pcp(sd_pgcl_prog, replacement_map, var_comp_dict, var_bound_dict, M_str, filtering=False):
     """
     Convert a bounded k-d PCP to a 1-d PCP.
-    
-    :param sd_pgcl_prog: A bounded k-d PCP object.
-    :param replacement_map: A dictionary mapping guard labels (e.g., "guard_0") to their original symbolic expressions.
-    :param var_comp_dict: A dictionary mapping each variable to the minimum value that must be added to make it non-negative.
-    :param var_bound_dict: A dictionary mapping each variable to the bound.
-    :param M_str: The unbounded variable. If no unbounded variable exists, this value will be None.
-    :return:
     """
-    # Calculate intermediate results, preparing for the convertion process.
+    if filtering:
+        # Filter and retain only meaningful annotation data for analysis
+        if M_str not in sd_pgcl_prog.variables:
+            M_str = None
+        var_comp_dict = {k: v for k, v in var_comp_dict.items() if k in sd_pgcl_prog.variables}
+        var_bound_dict = {k: v for k, v in var_bound_dict.items() if k in sd_pgcl_prog.variables}
+    
+    # Calculate intermediate results, preparing for the convertion process
     var_coeff_dict, var_sub_dict = get_bounded_coeffs(var_comp_dict, var_bound_dict, M_str)
     
-    # Compute and update the initial value of the new variable 'z_bd'.
+    # Compute and update the initial value of the new variable 'z_bd'
     init_val = 0
     for var, var_coeff in var_coeff_dict.items():
         if var != M_str:
@@ -100,5 +81,5 @@ def convert_bounded_pcp(sd_pgcl_prog, replacement_map, var_comp_dict, var_bound_
         init_val += var_coeff_dict[M_str] * sd_pgcl_prog.variables[M_str]
     sd_pgcl_prog.variables = {'z_bd': init_val}
     
-    # Transform the program's instructions by updating guards and assignments.
+    # Transform the program's instructions by updating guards and assignments
     convert_bounded_AST(sd_pgcl_prog.instructions, replacement_map, var_coeff_dict, var_sub_dict)
